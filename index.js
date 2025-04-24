6ef5f053631f9eb4c053c11f15bd9d7ffa6ab735
Alec Nono <alecbnono@gmail.com>
2025-04-24 17:08:46 +0800

    Add working product

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 "use strict";
 
+function colorBlack (e) {
+    e.target.style.backgroundColor = "black";
+}
+
 const grid = document.querySelector("#grid");
 
 let size = 16;
+let squareSide = 960/16;
 
 for (let i = 0; i < size * size; i++) {
-  const square = document.createElement("div");
+    const square = document.createElement("div");
+
+    square.style.width = `${squareSide}px`;
+    square.style.height = `${squareSide}px`;
+
+    square.classList.add("square");
+
+    grid.appendChild(square);
 }
+
+const squaresList = document.querySelectorAll(".square");
+
+console.log(squaresList);
+
+squaresList.forEach((e) => e.addEventListener("mouseenter", colorBlack));
+
